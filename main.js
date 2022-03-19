435fdc2754aee506c26bb352835a11f3d20277c5
Ashour100 <interinstitute5@gmail.com>
2022-03-19 17:05:10 +0100

    Bonus Added

--- main.js
+++ main.js
@@ -13,13 +13,18 @@ const app= new Vue({
                     done:false
                 });
                 this.content="";
+                this.listCount++;
             }
             else
                 alert('Write somthing');
         },
         removeItem: function(index){
             this.todoList.splice(index,1);
+            this.listCount--;
+        },
+        statusUpdate: function(index){
+            if(this.listCount>=1)
+            this.todoList[index].done=!this.todoList[index].done;
         }
-    }
-
+        }
 })
